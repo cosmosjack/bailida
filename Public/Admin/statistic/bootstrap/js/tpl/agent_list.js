e31a707049d12e5946e8e5e6cda0cd5e6c188812
Cosmos-jack <997823131@qq.com>
2017-09-05 15:30:48 +0800

    push

--- Public/Admin/statistic/bootstrap/js/tpl/agent_list.js
+++ Public/Admin/statistic/bootstrap/js/tpl/agent_list.js
@@ -5,12 +5,12 @@
 $(document).ready(function(){
     /* 获取 商品的列表 start */
     $.ajax({
-        url: AdminUrl+"/statisti/list", //&is_self=1  // 是否显示只显示自己直系管理的店铺 开关
+        url: AdminUrl+"/statisti/agent_list", //&is_self=1  // 是否显示只显示自己直系管理的店铺 开关
         type:"GET",
         data:{},
         success:function(msg){
-            alert(JSON.stringify(msg));
-            return false;
+            console.log(msg);
+            //return false;
             /* template.config("escape",false);
              var js_data = document.getElementById('js_data').innerHTML;
              var r = template(js_data,msg);
@@ -21,15 +21,15 @@ $(document).ready(function(){
                 //data 这里是固定不变的，name，position，salary，office 为你数据里对应的属性
                 //"ajax": AgentUrl+"/index.php?act=agent_user&op=index",
                 columns: [
-                    { data: 'store_name' },//店铺名字
-                    { data: 'relationship' }, //关系级别
-                    { data: 'agent_name' },//代理人名字
+                    { data: 'nickname' },//店铺名字
+                    { data: 'levelid' }, //关系级别
+                    { data: 'city' },//代理人名字
                     //{ data: 'store_user' },//联系人
                     /* { data:null,"defaultContent":"<span id='"+ this.a_id+"' value='' class='show_span'><em class='show_em'></em></span>",render:function(e){
                      console.log(e);
                      }},*/
                     { data:null,render:function(e){
-                        return "<span  value='' class='show_span'><em style='width:"+ e.point+";' class='show_em' ></em></span>";
+                        return "<span  value='' class='show_span'><em style='width:"+ e.exp+";' class='show_em' ></em></span>";
                     }},
                     { data: 'store_user_phone' },//电话
                     { data: null,
