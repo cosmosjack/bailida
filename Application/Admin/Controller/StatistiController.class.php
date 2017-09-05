95eec912e8c8e8f0e909150e91572448ccbfb255
Cosmos-jack <997823131@qq.com>
2017-09-05 11:45:23 +0800

    bootstrap

---
+++ Application/Admin/Controller/StatistiController.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by 大师兄
+ * 派系: 神秘剑派
+ * 技能: zxc秒杀
+ * Date: 2017/9/5
+ * Time: 10:21
+ * QQ:  997823131 
+ */
+namespace Admin\Controller;
+
+
+class StatistiController extends BaseController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        echo '统计的首页';
+        $this->display();
+    }
+}
