ec20da972378e2d0b8cc7dced43632be03bd9b6a
Cosmos-jack <997823131@qq.com>
2017-09-05 10:30:01 +0800

    dd

---
+++ Application/Admin/Controller/StatisticController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by 大师兄
+ * 派系: 神秘剑派
+ * 技能: zxc秒杀
+ * Date: 2017/9/5
+ * Time: 10:21
+ * QQ:  997823131 
+ */
+namespace Admin\Controller;
+class StatisticControl extends BaseController{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function index(){
+        $this->display();
+    }
+}
