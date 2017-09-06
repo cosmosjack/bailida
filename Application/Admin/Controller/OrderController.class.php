28fe9386b571cc72752d853973cc3f0efa39d8c7
Cosmos-jack <997823131@qq.com>
2017-09-06 14:53:21 +0800

    jack

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -18,7 +18,7 @@ class OrderController extends BaseController{
         $count      = $db_order->where($where)->count();// 查询满足要求的总记录数
         $Page       = new \Think\Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show       = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-        $list = $db_order->where('status=1')->order('create_time')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $list = $db_order->where($where)->limit($Page->firstRow.','.$Page->listRows)->select();
         $this->assign('list',$list);// 赋值数据集
         $this->assign('page',$show);// 赋值分页输出
         p($list);
