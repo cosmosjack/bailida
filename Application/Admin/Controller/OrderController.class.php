2e79c8abf58df38a5e8beee0278b42426d6f9b33
Cosmos-jack <997823131@qq.com>
2017-09-06 14:52:16 +0800

    jack

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -13,11 +13,18 @@ class OrderController extends BaseController{
         parent::__construct();
     }
     public function no_pass_order(){
-        $db_order = M("shop_order");
-        $data_order = $db_order
-            ->where(array('admin_ispass'=>array('eq',0)))
-            ->select();
-        p($data_order);
+        $where = array('admin_ispass'=>array('eq',0));
+        $db_order = M('shop_order'); // 实例化User对象
+        $count      = $db_order->where($where)->count();// 查询满足要求的总记录数
+        $Page       = new \Think\Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show       = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $list = $db_order->where('status=1')->order('create_time')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('list',$list);// 赋值数据集
+        $this->assign('page',$show);// 赋值分页输出
+        p($list);
+        p($show);
+        die;
+        $this->display(); // 输出模板
 
     }
 }
