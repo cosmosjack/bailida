35a22beafa876d292c23109017eae7f0b1ff1cb0
Cosmos-jack <997823131@qq.com>
2017-09-11 15:55:54 +0800

    jack

--- Application/Admin/Controller/StatistiController.class.php
+++ Application/Admin/Controller/StatistiController.class.php
@@ -186,6 +186,7 @@ class StatistiController extends BaseController
         $data_order = $db_order
             ->where($map)
             ->select();
+        p($data_order);
         $month_day_num = cal_days_in_month(CAL_GREGORIAN, $search_month, $search_year);
         if($data_order){
             // 声明 前端需要的 线形图 数据
