ea3a0809b30f3eddeb05141d1f642e82138364a9
Cosmos-jack <997823131@qq.com>
2017-09-13 10:17:49 +0800

    jack

---
+++ Application/Admin/Controller/AgentController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by 大师兄
+ * 派系: 神秘剑派
+ * 技能: zxc秒杀
+ * Date: 2017/9/13
+ * Time: 10:13
+ * QQ:  997823131 
+ */
+namespace Admin\Controller;
+class AgentController extends BaseController{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function set(){
+        $this->display();
+    }
+}
