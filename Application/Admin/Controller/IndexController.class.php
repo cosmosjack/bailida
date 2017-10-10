e637b72bc54b2e0fe22160ba4545878ecab5fd54
jack <997823131@qq.com>
2017-10-10 14:40:03 +0800

    jack

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -28,6 +28,7 @@ class IndexController extends BaseController
         foreach ($module as $k => $v) {
             $this->assign($v['name'], $v['name']);
         }
+        echo 'dddd';
         p($_SESSION['CMS']['user']['oath']);
         $this->display();
     }
